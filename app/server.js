0e3ad4577e009ad5b635631eb2bb600d471b0ece
cwong22 <cwong22@masonlive.gmu.edu>
2020-10-05 23:14:51 -0400

    Added basic server.js functionality

--- app/server.js
+++ app/server.js
@@ -3,36 +3,49 @@
 //use txt.TextChannel and usr.User to get the respective classes
 //as well as txt.ChannelMessage and usr.UserMessage
 
+var fs = require("fs");
 
 var txt = require("./textchannel.js");
 var usr = require("./user.js");
+
+var dataLocation = "../data/servers";
 
 class Server
 {
     serverName = "";
     serverID = 0;
     timeWhenBotWasAddedToServer = 0;
-    //you'll need some to way serialize this stuff  
 
-    constructor(serverName, serverID, timeWhenBotWasAddedToServer) {
+    textChannels = [];
+    users = [];
+
+    constructor(serverName) {
         this.serverName = serverName;
-        this.serverID = serverID;
-        this.timeWhenBotWasAddedToServer = timeWhenBotWasAddedToServer;
+        this.timeWhenBotWasAddedToServer = 0 + new Date(); //time in milliseconds
+        this.serverID = this.timeWhenBotWasAddedToServer; //rough implementation of unique identifier -- can be changed
+
+        this.textChannels = [];
+        this.users = [];
+
+        //also creates directories for server/textchannels/users upon initialization
+        serverLocation = dataLocation + this.serverName;
+        fs.mkdir(serverLocation);
+        fs.mkdir(serverLocation + "/textchannels");
+        fs.mkdir(serverLocation + "/users");
     }
     
-
-    addTextChannelToList()
+    //adds textchannel to local list
+    addTextChannelToList(textchannels)
     {
-
+        this.users.push(textchannels);
     }
 
-    addUserToList()
+    //adds user to local list
+    addUserToList(user)
     {
-
+        this.users.push(user);
     }
 
-    //contains an array of text channels
-    //also contains an array of users
 }
 
 module.exports = {Server};

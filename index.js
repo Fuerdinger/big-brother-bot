e1dea26fad53c69925140304091601ca672578e1
cwong22 <cwong22@masonlive.gmu.edu>
2020-10-07 23:42:00 -0400

    Tested and fixed server.js

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ var BigBrother = new bb.BigBrotherManager();
 
 
 
+/*  Testing code for server.js
+var srv = require('./app/server.js');
+
+//var Srv1 = new srv.Server("TestServer", "1", 1); //for creating a new server
+var Srv1 = new srv.Server("TestServer", null, null); // for creating an existing server object
+
+Srv1.addTextChannelToList("channel1", "2", 2);
+Srv1.addUserToList("user1", "3", 3);
+
+Srv1.cacheTextChannelMessage("2","hi", "3", 0);
+Srv1.cacheUserMessage("3", "hello", "2", 0);
+
+console.log(JSON.stringify(Srv1.getChannelMessages("channel1")));
+console.log(JSON.stringify(Srv1.getUserMessages("user1")));
+
+Srv1.allMessagesToMemory();
+*/
+
 
 /*  Testing code for user.js and textchannel.js
 var us = require('./app/user.js');

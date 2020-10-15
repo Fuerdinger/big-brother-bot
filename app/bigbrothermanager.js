d50429ec8dc984f802de8f3af3ce89c5a9b644ff
phungngo1020 <phungngo1020@gmail.com>
2020-10-15 14:56:33 -0400

    Fixed bigbrothermanager.js

--- app/bigbrothermanager.js
+++ app/bigbrothermanager.js
@@ -40,68 +40,107 @@ var data = JSON.parse(fs.readFileSync('./data/bigbrother.json', 'utf8'));
 
 class BigBrotherManager
 {
+    servers = [];
 
     constructor()
     {
-        var servers = data.map(s => s);
-        var serversSize = Object.keys(servers).length;
+        // var servers = data.map(s => s);
+        var serverstr = data.map(s => s);
+
+
+        // create a new server for each index
+        for (var i = 0; i < serverstr.length; i++) {
+            var server = new serv.Server(serverstr[i].json.serverName, serverstr[i].json.serverID, serverstr[i].json.timeBotWasAdded);
+            console.log(server);
+
+            this.servers.push(server);
+        }
 
 
         client.once('ready', () => {
             console.log('Ready!');
+            console.log('  ready');
+            console.log(this.servers.length);
         });
 
         client.on('message', message =>
-        {
-            var txtchannel = new txt.TextChannel(message.guild.name, message.channel.name, message.channel.id, message.channel.createdTimestamp);
-            txtchannel.recordMessage(message);
+        {   
+            var today = new Date();
+            var server = '';
+
+            // add message to Server - server.receiveMessage(message, channel.id, user.id, );
+            for (var i = 0; i < this.servers.length; i++) {
+                if (servers[i].serverID == message.guild.id) {
+                    server = servers[i];
+                }
+            }
+            if (server != '') {
+                server.catcherUserMessage(message.member.id, message, message.channel.id, today);
+            }
+        
+
+            // pass message to Server to determine if it's a command
+            // Server would then hand it off to UI to handle the command
+
+            // server.catcherUserMessage(user.id, message, channel.id);
+
         });
 
         /************ SERVERS ************/
-        client.on("guildCreate", function(guild) {
+        client.on('guildCreate', guild => {
             let newServer = new srv.Server(guild.name, guild.id);
-            servers.add(newServer);
+            this.servers.add(newServer);
         });
 
-        client.on("guildDelete", function(guild) {
+
+        client.on('guildDelete', guild => 
+        {
             let server = new srv.Server(guild.name, guild.id);
             for (let i = 0; i < this.servers.length; i++) {
                 if (server.id = guild.id) {
-                    serversay.splice(i, 1);
+                    this.servers.splice(i, 1);
                 }
             }
         });        
 
         /************ USERS ************/
-        client.on("guildMemberAdd", function(member){
+        client.on("guildMemberAdd", member => 
+        {
             /* Add users to servers list */
             let thisServer = this.servers.find(server => server.id == member.guild.id);
             thisServer.addUserToList(member.user.name, member.user.id, member.joinedTimestamp);
-
         });
+        // client.on("guildMemberRemove", member => 
+        // {
+        //     /* Add users to servers list */
+        //     let thisServer = this.servers.find(server => server.id == member.guild.id);
+            
+        // });
 
         /************ TEXT CHANNELS ************/
-        client.on("channelCreate", function(channel) 
+        client.on("channelCreate", channel => 
         {
-            // console.log(typeof(servers[0]));
-            // console.log(channel.guild.id);
-            let server = null;
-            server = servers.find(server => server.serverID == channel.guild.id);
-
-            /* Add channel to servers list */
-            for (var x = 0; x < serversSize; x++) {
-                if (server != null) {
-                    server.channels.push(channel.name);
-                    break;
+            let thisServer = '';
+            // thisServer = this.servers.find(server => server.serverID == channel.guild.id);
+            for (var i = 0; i < this.servers.length; i++) {
+                if (this.servers[i].json.serverID == channel.guild.id) {
+                    thisServer = this.servers[i];
                 }
             }
 
-            let bigbrother = JSON.stringify(servers);
-            fs.writeFileSync('./data/bigbrother.json', bigbrother);
+            /* Add channel to servers list */
+            for (var x = 0; x < this.servers.length; x++) {
+                if (thisServer != null) {
+                    thisServer.channels.push(channel.id);
+                    break;
+                }
+            }
+            if (thisServer != '') {
+                thisServer.addTextChannelToList(channel.name, channel.id, channel.createdTimestamp);
+            }
 
-            let thisServer = new srv.Server(channel.guild.name, channel.guild.id);
-
-            thisServer.addTextChannelToList(channel.name, channel.id, channel.createdTimestamp);
+            // let bigbrother = JSON.stringify(this.servers);
+            // fs.writeFileSync('./data/bigbrother.json', bigbrother);
 
         });
 
